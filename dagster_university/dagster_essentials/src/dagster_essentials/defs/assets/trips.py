972d12a25f70ced87414bd2aaad2f23cbf38ced3
John Rullo <john.rullo@asterinsights.com>
2026-03-02 09:45:48 -0500

    Updated files
    Final files for dagster essentials

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -0,0 +1,112 @@
+import requests
+from dagster_duckdb import DuckDBResource
+from dagster_essentials.defs.assets import constants
+from dagster_essentials.defs.partitions import monthly_partition
+import dagster as dg
+import pandas as pd
+
+# src/dagster_essentials/defs/assets/trips.py
+@dg.asset(
+    partitions_def=monthly_partition,
+    group_name="raw_files",
+)
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
+    """The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal."""
+
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+# src/dagster_essentials/defs/assets/trips.py
+# src/dagster_essentials/defs/assets/trips.py
+import dagster as dg
+
+@dg.asset(
+    group_name="raw_files",
+)
+def taxi_zones_file() -> dg.MaterializeResult:
+    """
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
+    num_rows = len(pd.read_csv(constants.TAXI_ZONES_FILE_PATH))
+
+    return dg.MaterializeResult(
+        metadata={
+            'Number of records': dg.MetadataValue.int(num_rows)
+        }
+    )
+
+
+# src/dagster_essentials/defs/assets/trips.py
+@dg.asset(
+    deps=["taxi_trips_file", "taxi_zones"],
+    partitions_def=monthly_partition,
+    group_name="ingested",
+)
+def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
+  """The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month."""
+
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
+
+  query = f"""
+    create table if not exists trips (
+      vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+      rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+      pickup_datetime timestamp, trip_distance double, passenger_count double,
+      total_amount double, partition_date varchar
+    );
+
+    delete from trips where partition_date = '{month_to_fetch}';
+
+    insert into trips
+    select
+      VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+      tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+    from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+  """
+
+  with database.get_connection() as conn:
+      conn.execute(query)
+
+
+# src/dagster_essentials/defs/assets/zones.py
+@dg.asset(
+    deps=["taxi_zones_file"],
+    group_name="ingested",
+)
+def taxi_zones(database: DuckDBResource) -> None:
+    """The raw taxi zones dataset, loaded into a DuckDB database"""
+    query = f"""
+        create or replace table zones as (
+          select
+            LocationID as zone_id,
+            zone,
+            borough,
+            the_geom as geometry,
+          from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    try:
+        with database.get_connection() as conn:
+            conn.execute(query)
+    except conn.DuckDBException as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Ensure the connection is closed in the finally block
+        if conn:
+            conn.close()
+
---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/resources.py
@@ -0,0 +1,12 @@
+# src/dagster_essentials/defs/resources.py
+from dagster_duckdb import DuckDBResource
+import dagster as dg
+
+database_resource = DuckDBResource(
+    database=dg.EnvVar("DUCKDB_DATABASE")
+)
+
+@dg.definitions
+def resources() -> dg.Definitions:
+    return dg.Definitions(resources={"database": database_resource})
+
